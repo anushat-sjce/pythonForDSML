63f14376cde423d9a1a0ab22a02ba1873e181a8c
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-08-05 10:23:15 +0530

    Create addfunc.py

---
+++ addFunction/addfunc.py
@@ -0,0 +1,10 @@
+"""
+This module performs addition of two numbers
+"""
+
+def addition(a, b):
+    """Add two integers"""
+    return a+b
+
+summation = addition(20, 20)
+print("Sum of two numbers :", summation)
