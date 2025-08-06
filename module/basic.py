9f5347a71501a62a45e93e7cf03f7b9316018311
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-08-06 11:12:32 +0530

    Create basic.py

---
+++ module/basic.py
@@ -0,0 +1,18 @@
+def square(a):
+    """
+    This function returns square of a number
+    """
+    return (a*a)
+
+def doubler(b):
+    """
+    This function returns result as twice the input
+    variable
+    """
+    return (2*b)
+
+def addition(a, b):
+    """
+    This fucntion returns sum of a and b
+    """
+    return (a + b)
