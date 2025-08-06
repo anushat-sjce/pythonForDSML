92dbd0053771c0904b1c19ca3f3d2b5dbf282c8d
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-08-06 13:45:01 +0530

    Update server.py

--- flaskexample/server.py
+++ flaskexample/server.py
@@ -12,8 +12,21 @@ def home():
 
 
 
-### How to run
+### How to run terminals
   """
-  flask --app server --debug run 
-  curl -X GET -i -w '\n' localhost:5000
+  flask --app server --debug run - 1st terminal
+  curl -X GET -i -w '\n' localhost:5000 - 2nd terminal
   """
+
+
+#import the Flask class from the flask module
+from flask import Flask
+
+#create instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+#define a route for the root URL ("/")
+@app.route("/")
+def home():
+    #Function that handles requests to the root URL
+    return {"Message":"Hello world", "From":"Anusha", "City":"Bangalore"}
