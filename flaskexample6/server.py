fb73a4cf9224ca0d69617f4d73b17f24e66b5320
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-08-07 13:35:16 +0530

    Create server.py

---
+++ flaskexample6/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+data = [ 
+    {
+        "uuid" : 1,
+        "First Name" : "Anusha",
+        "Last Name ": "T "
+    }, 
+    {
+        "uuid" : 2,
+        "First Name" : "Jayathirth",
+        "Last Name" : "Rao"
+    }
+]
+
+@app.route("/person/<int:uuid>")
+
+def get_person_byid(uuid):
+    for person in data:
+        if person["uuid"] == uuid:
+            return person
+    return {"Message": "Person not found"}
