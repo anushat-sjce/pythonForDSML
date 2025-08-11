eb04cfc24192757ad3bb54ea74d5d569de507209
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-08-11 11:36:21 +0530

    Create mathematics.py

---
+++ flaskExample8/mathematics.py
@@ -0,0 +1,12 @@
+"""
+    This function returns the sum of two numbers
+"""
+def summation(a, b):
+    return (a+b)
+
+def subtraction(a, b):
+    return (a-b)
+
+def multiplication(a, b):
+    return (a*b)
+    
